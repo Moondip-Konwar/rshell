cbd98d9d6d3f3b97a3f423ec9fffd39e43885d16
Moondip Konwar <moondipkonwar9@gmail.com>
2026-02-25 10:49:01 +0530

    Input loop + Exit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{self, Write};
+use std::process::exit;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let input = get_input();
+        process_input(input);
+    }
+}
+
+fn get_input() -> String {
+    let mut input: String = String::new();
+    print!("$ ");
+    io::stdout().flush().unwrap();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line.");
+    input = input.trim().to_string();
+
+    input
+}
+
+fn process_input(input: String) {
+    if input.is_empty() {
+        return;
+    }
+
+    match input.as_str() {
+        "exit" => exit(0),
+        "cd" => println!("Not implemented yet."),
+        _ => {
+            println!("{}: Command not found.", input)
+        }
+    }
 }
